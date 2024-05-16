7744d88f31062bb73172c88879915cb0c5789c90
JB Wood <jb@jbwood.io>
2024-05-15 22:53:58 -0400

    add branding

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -4,21 +4,6 @@ defmodule Carddo.AccountsFixtures do
   entities via the `Carddo.Accounts` context.
   """
 
-  @doc """
-  Generate a user.
-  """
-  def user_fixture(attrs \\ %{}) do
-    {:ok, user} =
-      attrs
-      |> Enum.into(%{
-        email: "some email",
-        username: "some username"
-      })
-      |> Carddo.Accounts.create_user()
-
-    user
-  end
-
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
 
