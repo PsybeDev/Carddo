a8acc771edcf287ac2b2769122eccd5fb8d0af8e
JB Wood <jb@jbwood.io>
2025-03-27 11:53:59 -0400

    Active nav (#24)
    * add nav.ex to manage active nav
    
    * fix bug with missing active_nav in assigns

--- lib/carddo_web/controllers/page_controller.ex
+++ lib/carddo_web/controllers/page_controller.ex
@@ -4,6 +4,8 @@ defmodule CarddoWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    # ensure active_nav is part of assigns.
+    conn = assign(conn, :active_nav, conn.assigns[:active_nav] || nil)
     render(conn, :home, layout: false)
   end
 end
--- lib/carddo_web/router.ex
+++ lib/carddo_web/router.ex
@@ -51,7 +51,7 @@ defmodule CarddoWeb.Router do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
     live_session :redirect_if_user_is_authenticated,
-      on_mount: [{CarddoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      on_mount: [{CarddoWeb.UserAuth, :redirect_if_user_is_authenticated}, CarddoWeb.Nav] do
       live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
       live "/users/reset_password", UserForgotPasswordLive, :new
@@ -65,7 +65,7 @@ defmodule CarddoWeb.Router do
     pipe_through [:browser, :require_authenticated_user]
 
     live_session :require_authenticated_user,
-      on_mount: [{CarddoWeb.UserAuth, :ensure_authenticated}] do
+      on_mount: [{CarddoWeb.UserAuth, :ensure_authenticated}, CarddoWeb.Nav] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
 
@@ -84,7 +84,7 @@ defmodule CarddoWeb.Router do
     delete "/users/log_out", UserSessionController, :delete
 
     live_session :current_user,
-      on_mount: [{CarddoWeb.UserAuth, :mount_current_user}] do
+      on_mount: [{CarddoWeb.UserAuth, :mount_current_user}, CarddoWeb.Nav] do
       live "/users/confirm/:token", UserConfirmationLive, :edit
       live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
