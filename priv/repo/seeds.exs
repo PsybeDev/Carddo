4ce9ad7e88bcbd7a95925241e6e245c06691b4f5
JB Wood <jb@jbwood.io>
2025-05-22 21:48:51 -0400

    Add Daisy UI [Jb/car 10 daisyui] (#25)
    * add DaisyUi, update nav to use daisyui
    
    * add seeds, update core components and menu
    
    * update list
    
    * remove text color on lists

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Carddo.Accounts.User
+alias Carddo.Repo
+alias Carddo.Accounts
+alias Carddo.Games
+
+# Clear DB
+Repo.delete_all User
+Repo.delete_all Games.Game
+Repo.delete_all Games.Format
+
+user = Accounts.register_user(%{
+  username: "user",
+  email: "user@example.com",
+  password: "random_password",
+  confirmed_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+})
+
+Accounts.register_user(%{
+  username: "admin",
+  email: "admin@example.com",
+  password: "random_password",
+  confirmed_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+})
+
+Games.create_game(%{
+  name: "Super Cool TCG",
+  description: "A super cool trading card game",
+})
