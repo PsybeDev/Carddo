0a471757ac3a0200e060a62331bcb7100a6610ad
Jay Ruzic <jb@jbwood.io>
2026-03-02 09:56:56 -0500

    init backend

---
+++ backend/lib/carddo_web/router.ex
@@ -0,0 +1,28 @@
+defmodule CarddoWeb.Router do
+  use CarddoWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", CarddoWeb do
+    pipe_through :api
+  end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:carddo, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      live_dashboard "/dashboard", metrics: CarddoWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
