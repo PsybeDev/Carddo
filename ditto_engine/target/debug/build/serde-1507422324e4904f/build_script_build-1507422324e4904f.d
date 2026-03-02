efa19cb5dbd14407c877bab1a15d84f42cb4ce67
JB Wood <jb@jbwood.io>
2026-03-02 13:41:05 -0500

    init ditto engine (#58)

---
+++ ditto_engine/target/debug/build/serde-1507422324e4904f/build_script_build-1507422324e4904f.d
@@ -0,0 +1,5 @@
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/build/serde-1507422324e4904f/build_script_build-1507422324e4904f.d: /home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
+
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/build/serde-1507422324e4904f/build_script_build-1507422324e4904f: /home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
+
+/home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:
--- makefile
+++ makefile
@@ -1,6 +1,12 @@
+.PHONY: setup dev dev-backend dev-frontend build-wasm
+
 setup:
 	cd backend && mix deps.get
 	cd frontend && pnpm i
+	$(MAKE) build-wasm
+
+build-wadm:
+	cd ditto_engine/ditto_wasm && wasm-pack build --target web
 
 dev-backend:
 	cd backend && mix phx.server
