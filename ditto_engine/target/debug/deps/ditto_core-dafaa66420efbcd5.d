8e519a583b4d6040c165500ab031b0cad0e63c0b
Jay Ruzic <jb@jbwood.io>
2026-03-02 13:36:07 -0500

    init ditto engine

---
+++ ditto_engine/target/debug/deps/ditto_core-dafaa66420efbcd5.d
@@ -0,0 +1,5 @@
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/deps/ditto_core-dafaa66420efbcd5.d: ditto_core/src/lib.rs
+
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/deps/libditto_core-dafaa66420efbcd5.rmeta: ditto_core/src/lib.rs
+
+ditto_core/src/lib.rs:
---
+++ ditto_engine/target/debug/deps/inventory-f244b6cd2f2dc0cc.d
@@ -0,0 +1,5 @@
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/deps/inventory-f244b6cd2f2dc0cc.d: /home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.22/src/lib.rs
+
+/home/psybe/Projects/Carddo/ditto_engine/target/debug/deps/libinventory-f244b6cd2f2dc0cc.rmeta: /home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.22/src/lib.rs
+
+/home/psybe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.22/src/lib.rs:
